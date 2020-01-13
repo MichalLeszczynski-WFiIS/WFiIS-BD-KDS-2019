a5ca8646aad9928b4af40da2d56f8392277bbd2a
MichalLeszczynski <mleszczynsk@gmail.com>
2020-01-13 11:23:45 +0100

    Remove old file

--- docker_clean.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-
-DOCKER_IMAGE=katalog-dziel-sztuki
-
-docker container stop $(docker container ls |grep $DOCKER_IMAGE| cut -d" " -f1)
-docker container rm $(docker container ls -a|grep $DOCKER_IMAGE| cut -d" " -f1)
-docker image rm $(docker image ls | grep $DOCKER_IMAGE | awk '{print $3}')
